d9af7b04232e0dbc84e645547103f5686496181c
peppino25 <giuseppecroce33@gmail.com>
2025-11-17 22:20:47 +0100

    Bug fix and added table replace data

--- src/components/PTable.jsx
+++ src/components/PTable.jsx
@@ -6,27 +6,9 @@ export default function PTable({ incoming_data }) {
     const punteggiContainerRef = useRef(null);
     const punteggiTableRef = useRef(null);
 
-
   useEffect(() => {
-    if (!incoming_data || !incoming_data.punteggi) return;
-    const currentPunteggi = incoming_data.punteggi;
-
-    const tableData = Object.entries(currentPunteggi).map(([pe, range]) => ({
-        PE: pe,
-        Range: range,
-    }));
-
-    if (punteggiTableRef.current && typeof punteggiTableRef.current.destroy === "function") {
-        try {
-            punteggiTableRef.current.destroy();
-        } catch (err) {
-            console.warn("Tabulator destroy failed:", err);
-        }
-        punteggiTableRef.current = null;
-    }
-
     punteggiTableRef.current = new Tabulator(punteggiContainerRef.current, {
-        data: tableData,
+        data: [],
         layout: "fitDataTable",
         rowHeight: 40,
         placeholder: "Nessun punteggio disponibile",
@@ -65,7 +47,25 @@ export default function PTable({ incoming_data }) {
                 punteggiTableRef.current = null;
             }
         };
-    }, [incoming_data]);
+    }, []);
+
+    useEffect(() => {
+        const table = punteggiTableRef.current;
+        if (!table) return;
+        if (!incoming_data || !incoming_data.punteggi) return;
+
+        const currentPunteggi = incoming_data.punteggi;
+        console.log(currentPunteggi);
+
+        const tableData = Object.entries(currentPunteggi).map(([pe, range]) => ({
+            PE: pe,
+            Range: range,
+        }));
+
+        punteggiTableRef.current.replaceData(tableData);
+
+
+    }, [incoming_data])
 
     return <div ref={punteggiContainerRef}/>;
 }
