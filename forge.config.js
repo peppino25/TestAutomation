0b74aace25f49c24711a41591b1f6a1dfe60fec4
peppino25 <giuseppecroce33@gmail.com>
2025-11-19 13:54:37 +0100

    Rollback

--- forge.config.js
+++ forge.config.js
@@ -1,11 +1,15 @@
+const { FusesPlugin } = require('@electron-forge/plugin-fuses');
+const { FuseV1Options, FuseVersion } = require('@electron/fuses');
+require("dotenv").config();
+
+
 module.exports = {
   packagerConfig: {
     asar: true,
   },
+  rebuildConfig: {},
   makers: [
-    {
-      name: '@electron-forge/maker-dmg',
-    },
+    { name: '@electron-forge/maker-dmg' },
   ],
   plugins: [
     {
@@ -31,5 +35,21 @@ module.exports = {
         ],
       },
     },
+
+    // ✅ Fuses should only be applied in packaged apps, not during "start"
+    ...(process.env.MODE === 'production'
+      ?
+      [
+          new FusesPlugin({
+            version: FuseVersion.V1,
+            [FuseV1Options.RunAsNode]: false,
+            [FuseV1Options.EnableCookieEncryption]: true,
+            [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
+            [FuseV1Options.EnableNodeCliInspectArguments]: false,
+            [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
+            [FuseV1Options.OnlyLoadAppFromAsar]: true,
+          }),
+        ]
+      : []),
   ],
 };
