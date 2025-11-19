527df203983f1def16cf162289ae833696f6e760
peppino25 <giuseppecroce33@gmail.com>
2025-11-19 13:16:11 +0100

    Added macos compatibility

--- forge.config.js
+++ forge.config.js
@@ -1,15 +1,11 @@
-const { FusesPlugin } = require('@electron-forge/plugin-fuses');
-const { FuseV1Options, FuseVersion } = require('@electron/fuses');
-require("dotenv").config();
-
-
 module.exports = {
   packagerConfig: {
     asar: true,
   },
-  rebuildConfig: {},
   makers: [
-    { name: '@electron-forge/maker-dmg', config: {} },
+    {
+      name: '@electron-forge/maker-dmg',
+    },
   ],
   plugins: [
     {
@@ -35,21 +31,5 @@ module.exports = {
         ],
       },
     },
-
-    // ✅ Fuses should only be applied in packaged apps, not during "start"
-    ...(process.env.MODE === 'production'
-      ?
-      [
-          new FusesPlugin({
-            version: FuseVersion.V1,
-            [FuseV1Options.RunAsNode]: false,
-            [FuseV1Options.EnableCookieEncryption]: true,
-            [FuseV1Options.EnableNodeOptionsEnvironmentVariable]: false,
-            [FuseV1Options.EnableNodeCliInspectArguments]: false,
-            [FuseV1Options.EnableEmbeddedAsarIntegrityValidation]: true,
-            [FuseV1Options.OnlyLoadAppFromAsar]: true,
-          }),
-        ]
-      : []),
   ],
 };
